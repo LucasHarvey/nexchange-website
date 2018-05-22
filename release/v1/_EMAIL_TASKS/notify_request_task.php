2639dae16cba4a31b94653e100e94f44910c88c3
Lucas <lucas.harvey10@gmail.com>
2018-05-22 15:39:12 -0400

    quotation marks

--- release/v1/_EMAIL_TASKS/notify_request_task.php
+++ release/v1/_EMAIL_TASKS/notify_request_task.php
@@ -4,12 +4,11 @@ include_once("./_EMAIL_TASKS/_generic_email.php");
 
 function request_email($conn, $firstName, $lastName, $email, $subject, $message){
 
-    $emailMessage = "New Request from $firstName $lastName:\r\n$message\r\nReply at the following email address: $email";
+    $emailMessage = "New Request from ".$firstName." ".$lastName.":\r\n".$message."\r\nReply at the following email address: ".$email;
 
     $to = "lucas.harvey10@gmail.com";
 
-    return send_email($conn, $to, $subject, $emailMessage, true);
+    return send_email($conn, $to, htmlentities($subject), htmlentities($emailMessage), true);
 }
-
 
 ?>
